56f99b94c53afb7df3adeffc242b0ee054bdaab0
NikcyWan <w_nicky@qq.com>
2016-11-02 16:25:43 +0800

    Initial Commit

---
+++ CustomEmojiKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomEmojiKeyboard
+//
+//  Created by NickyWan on 2016/11/2.
+//  Copyright © 2016年 NickyWan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomEmojiKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomEmojiKeyboard
+//
+//  Created by NickyWan on 2016/11/2.
+//  Copyright © 2016年 NickyWan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
